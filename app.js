07969093c6d6879a69ad4374149b624447a02cb1
Dmitry Shutov <7dmitry77@gmail.com>
2017-08-09 18:36:23 +0400

    test email

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('http').createServer();
 const mongo = require('./db');
 const socket = require('socket.io')(app);
 mongo.connect();
-//app.listen(3100);
+app.listen(3100);
 
 socket.on('connection', () => {
     console.log('connect to socket');
